280b7a3bb7f6c66ec595affdeace2306ec664ca0
ezequielvalente-cyber <souza.silva.daniel1504@escola.pr.gov.br>
2026-06-01 11:04:14 -0300

    segundo commit

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(express.json());
 app.use(cors());
 
 //http://localhost:3000/saudacao?nome=maria
-app.get("/saudacao", (req,res)=>{
+app.get("/saudacao", (req, res) => {
     const nome = req.query.nome;
-    if(!nome){
+    if (!nome) {
         return res.status(404).json(
             {
                 erro: "nome não informado"
@@ -18,36 +18,48 @@ app.get("/saudacao", (req,res)=>{
     }
     res.json(
         {
-        mensagem: `saudação ${nome}!`
+            mensagem: `saudação ${nome}!`
         }
     )
 })
-app.post("/imc",(req,res) =>{
-    const { nome, idade, altura, peso}= req.body;
+app.post("/imc", (req, res) => {
+    const { nome, idade, altura, peso } = req.body;
 
-    if(!nome || !idade || !altura || !peso){
-        return res.status(404).json({erro: "dados imcompletos"})
+    if (!nome || !idade || !altura || !peso) {
+        return res.status(404).json({ erro: "dados imcompletos" })
     }
-    const imc = peso / (altura*altura);
+    const imc = peso / (altura * altura);
     res.json({
         nome,
         idade,
         imc: imc.toFixed(2)
     })
 })
-app.post("/media",(req,res) =>{
-    const {nota1, nota2}= req.body;
+app.post("/media", (req, res) => {
+    const { nota1, nota2 } = req.body;
 
-    if( !nota1 || !nota2){
-        return res.status(404).json({erro: "dados incompletos"})
+    if (!nota1 || !nota2) {
+        return res.status(404).json({ erro: "dados incompletos" })
     }
-    const media = (parseFloat(nota1) + parseFloat(nota2))/2;
-    
+    const media = (parseFloat(nota1) + parseFloat(nota2)) / 2;
+
     res.json({
-        mensagem:media >=7 ? "aprovado" : "reprovado",
+        mensagem: media >= 7 ? "aprovado" : "reprovado",
         media: parseFloat(media)
     })
-   
+
+})
+app.post("/login", (req, res) => {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+        return res.status(404).json({ erro: "dados incompletos" })
+    }
+     if (email === "admin@admin.com" && senha === "123456") {
+        return res.status(200).json({ token: "123456" })
+    } else {
+        return res.status(404).json({ erro: "senha ou email incorreto" })
+    }
 })
 //finalzao
 app.listen(port, () => {
