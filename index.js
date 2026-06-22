eb4e9b27eefdfd1aee9231f6fc9bcab4995bd722
ezequielvalente-cyber <souza.silva.daniel1504@escola.pr.gov.br>
2026-06-22 12:48:44 -0300

    terceiro commit

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const path = require("path"); //cuida dos caminhos do seu arquivo, para ele ser achado independente de onde esteja salvo
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
+
+const clientesFile = path.join(__dirname, "clientes.json");
+function salvarClientes(clientes){
+    fs.writeFileSync(clientesFile, JSON.stringify(clientes, null, 2),"utf-8")
+}
+
+function lerClientes(){//ver se o clientes que o front está mandando já existe no sistema
+    if (!fs.existsSync.clientesFile){// fs biblioteca, existtsSync é uma função da biblioteca para ver se o arquivo já existe
+        return[];//retorna um array vazio
+    }
+    const dados = fs.readFileSync(clientesFile, 'utf-8')//vai ler o arquivo e escrever ele dentro de dados com acentos e etc
+    try{
+        return JSON.parse(dados) || [] // a função parse transforma a string em json e o || faz com que se na hora de transformar o dados dar erro, ele retorna um array vazio
+    }
+    catch(e){
+        return []// caso der erro ele retorna um arry vazio
+    }
+}
+
+app.post("/clientes", (req, res) =>{
+    const { nome, cpf, cep, rua, cidade, estado, numero } = req.body;
+    if (!nome || !cpf || !cep){
+        return res.status(404).json({erro: "dados incompletos"})
+    }
+    const clientes = lerClientes(); //todos os clientes serão armazenados na constante clientes
+    if (clientes.some(c=>c.cpf===cpf)){ //o c funciona como uma variavel de posição, que vai tomar cada posição de linha da const clientes e verificar se é igual ao cpf que está vindo
+        return res.status(404).json({erro: "Clientes já cadastrado"})
+    }
+    const novoCliente = {nome, cpf, cep, rua, cidade, estado, numero};//se o cliente for novo, ele será salvo na cost novoCliente
+    clientes.push(novoCliente);// a função push vai salvar todos os dados dentro da const clientes sem excluir os clientes já cadastrados anteriormente, formando uma lista com os clienes antigos mais o novo
+    salvarClientes(clientes)//vai salvar clientes dentro da função salvaClientes
+    return res.status(201).json({mensagem: "cliente cadastrado com sucesso"})
+})
 
 //http://localhost:3000/saudacao?nome=maria
 app.get("/saudacao", (req, res) => {
